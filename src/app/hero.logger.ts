623e4a892ef4b09ab1c85eca543a95d463106fd3
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-04-04 01:19:07 +0200

    refactor: use ScopedLumberjackLogger

--- src/app/hero.logger.ts
+++ src/app/hero.logger.ts
@@ -1,37 +1,31 @@
 import { Injectable } from '@angular/core';
-import { LumberjackLogger } from '@ngworker/lumberjack';
+import { ScopedLumberjackLogger } from '@ngworker/lumberjack';
 
 import { Hero } from './hero';
 
 @Injectable({
   providedIn: 'root',
 })
-export class HeroLogger extends LumberjackLogger {
+export class HeroLogger extends ScopedLumberjackLogger {
+  scope = 'HeroService';
+
   addedHero(id: number): void {
-    const logger = this.createTraceLogger(`added hero w/ id=${id}`)
-      .withScope('HeroService')
-      .build();
+    const logger = this.createTraceLogger(`added hero w/ id=${id}`).build();
     logger();
   }
 
   deletedHero(id: number): void {
-    const logger = this.createTraceLogger(`deleted hero id=${id}`)
-      .withScope('HeroService')
-      .build();
+    const logger = this.createTraceLogger(`deleted hero id=${id}`).build();
     logger();
   }
 
   fetchedHero(id: number): void {
-    const logger = this.createTraceLogger(`fetched hero id=${id}`)
-      .withScope('HeroService')
-      .build();
+    const logger = this.createTraceLogger(`fetched hero id=${id}`).build();
     logger();
   }
 
   fetchedHeroes(): void {
-    const logger = this.createTraceLogger('fetched heroes')
-      .withScope('HeroService')
-      .build();
+    const logger = this.createTraceLogger('fetched heroes').build();
     logger();
   }
 
@@ -40,34 +34,26 @@ export class HeroLogger extends LumberjackLogger {
       heroes.length > 0
         ? `found heroes matching "${term}"`
         : `no heroes matching "${term}"`;
-    const logger = this.createTraceLogger(message)
-      .withScope('HeroService')
-      .build();
+    const logger = this.createTraceLogger(message).build();
     logger();
   }
 
   operationFailed(errorMessage: string, operation = 'operation'): void {
     const logger = this.createErrorLogger(
       `${operation} failed: ${errorMessage}`
-    )
-      .withScope('HeroService')
-      .build();
+    ).build();
     logger();
   }
 
   tryFetchHero(id: number, hero: Hero | undefined): void {
     const outcome = hero ? `fetched` : `did not find`;
     const message = `${outcome} hero id=${id}`;
-    const logger = this.createTraceLogger(message)
-      .withScope('HeroService')
-      .build();
+    const logger = this.createTraceLogger(message).build();
     logger();
   }
 
   updatedHero(id: number): void {
-    const logger = this.createTraceLogger(`updated hero id=${id}`)
-      .withScope('HeroService')
-      .build();
+    const logger = this.createTraceLogger(`updated hero id=${id}`).build();
     logger();
   }
 }
