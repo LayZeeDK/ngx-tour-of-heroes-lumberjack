9c53618b478e72ab19a5344549f29e4ace939771
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-04-04 00:09:02 +0200

    feat: use LumberjackService for logs

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,54 +1,53 @@
-import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+import { Injectable } from '@angular/core';
+import { LumberjackLevel, LumberjackLogLevel, LumberjackService, LumberjackTimeService } from '@ngworker/lumberjack';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
 
 import { Hero } from './hero';
 import { MessageService } from './message.service';
 
-
 @Injectable({ providedIn: 'root' })
 export class HeroService {
-
-  private heroesUrl = 'api/heroes';  // URL to web api
+  private heroesUrl = 'api/heroes'; // URL to web api
 
   httpOptions = {
-    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
   constructor(
     private http: HttpClient,
-    private messageService: MessageService) { }
+    private messageService: MessageService,
+    private lumberjack: LumberjackService,
+    private lumberjackTime: LumberjackTimeService
+  ) {}
 
   /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
-    return this.http.get<Hero[]>(this.heroesUrl)
-      .pipe(
-        tap(_ => this.log('fetched heroes')),
-        catchError(this.handleError<Hero[]>('getHeroes', []))
-      );
+    return this.http.get<Hero[]>(this.heroesUrl).pipe(
+      tap((_) => this.log('fetched heroes', LumberjackLevel.Trace)),
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+    );
   }
 
   /** GET hero by id. Return `undefined` when id not found */
   getHeroNo404<Data>(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/?id=${id}`;
-    return this.http.get<Hero[]>(url)
-      .pipe(
-        map(heroes => heroes[0]), // returns a {0|1} element array
-        tap(h => {
-          const outcome = h ? `fetched` : `did not find`;
-          this.log(`${outcome} hero id=${id}`);
-        }),
-        catchError(this.handleError<Hero>(`getHero id=${id}`))
-      );
+    return this.http.get<Hero[]>(url).pipe(
+      map((heroes) => heroes[0]), // returns a {0|1} element array
+      tap((h) => {
+        const outcome = h ? `fetched` : `did not find`;
+        this.log(`${outcome} hero id=${id}`, LumberjackLevel.Trace);
+      }),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
     const url = `${this.heroesUrl}/${id}`;
     return this.http.get<Hero>(url).pipe(
-      tap(_ => this.log(`fetched hero id=${id}`)),
+      tap((_) => this.log(`fetched hero id=${id}`, LumberjackLevel.Trace)),
       catchError(this.handleError<Hero>(`getHero id=${id}`))
     );
   }
@@ -60,9 +59,11 @@ export class HeroService {
       return of([]);
     }
     return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
-      tap(x => x.length ?
-         this.log(`found heroes matching "${term}"`) :
-         this.log(`no heroes matching "${term}"`)),
+      tap((x) =>
+        x.length
+          ? this.log(`found heroes matching "${term}"`, LumberjackLevel.Trace)
+          : this.log(`no heroes matching "${term}"`, LumberjackLevel.Trace)
+      ),
       catchError(this.handleError<Hero[]>('searchHeroes', []))
     );
   }
@@ -72,7 +73,9 @@ export class HeroService {
   /** POST: add a new hero to the server */
   addHero(hero: Hero): Observable<Hero> {
     return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
-      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      tap((newHero: Hero) =>
+        this.log(`added hero w/ id=${newHero.id}`, LumberjackLevel.Trace)
+      ),
       catchError(this.handleError<Hero>('addHero'))
     );
   }
@@ -82,7 +85,7 @@ export class HeroService {
     const url = `${this.heroesUrl}/${id}`;
 
     return this.http.delete<Hero>(url, this.httpOptions).pipe(
-      tap(_ => this.log(`deleted hero id=${id}`)),
+      tap((_) => this.log(`deleted hero id=${id}`, LumberjackLevel.Trace)),
       catchError(this.handleError<Hero>('deleteHero'))
     );
   }
@@ -90,7 +93,7 @@ export class HeroService {
   /** PUT: update the hero on the server */
   updateHero(hero: Hero): Observable<any> {
     return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
-      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      tap((_) => this.log(`updated hero id=${hero.id}`, LumberjackLevel.Trace)),
       catchError(this.handleError<any>('updateHero'))
     );
   }
@@ -103,12 +106,7 @@ export class HeroService {
    */
   private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
-
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
+      this.log(`${operation} failed: ${error.message}`, LumberjackLevel.Error);
 
       // Let the app keep running by returning an empty result.
       return of(result as T);
@@ -116,7 +114,14 @@ export class HeroService {
   }
 
   /** Log a HeroService message with the MessageService */
-  private log(message: string) {
-    this.messageService.add(`HeroService: ${message}`);
+  private log(message: string, level: LumberjackLogLevel) {
+    this.lumberjack.log({
+      createdAt: this.lumberjackTime.getUnixEpochTicks(),
+      level,
+      message,
+      scope: 'HeroService',
+    });
+    // TODO: add custom log driver for messages component
+    // this.messageService.add(`HeroService: ${message}`);
   }
 }
